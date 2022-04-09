876539f8ebec25d4dad698e1b948a5c82f0b9313
Manuel <barrma.it@gmail.com>
2022-04-08 21:00:59 -0700

    init

---
+++ terraform/jenkins/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+variable "name" {
+    description = "Name the instance on deploy"
+}
+
+resource "aws_instance" "devops_01_jenkins" {
+    ami = "ami-04505e74c0741db8d"
+    instance_type = "t2.micro"
+    key_name = "devops_nopal"
+
+    tags = {
+        Name = "${var.name}"
+    }
+}
